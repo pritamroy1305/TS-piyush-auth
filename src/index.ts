eee8d212073573010f77f3f960b205b925770de7
Pritam <pritamroy13052003@gmail.com>
2026-04-18 01:03:06 +0530

    feat: initialize Express application and server setup
    - Added a new file `src/app/index.ts` to define the Express application with a welcome route.
    - Updated `src/index.ts` to create and start an HTTP server using the Express application.
    - Modified `tsconfig.json` to enable Node types and adjust module syntax settings.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("HELLO hhh");
+import {createServer } from 'node:http'
+import { createApplication} from './app'
+async function main ()
+{
+    try {
+
+        const server = createServer(createApplication())
+        const PORT: number = 8080
+
+        server.listen(PORT , () => {
+            console.log(`http server is running on port ${PORT}`);
+            
+        })
+        
+    } catch (error) {
+        console.log("ERROR IN STARTING ");
+        
+        
+    }
+
+}
+
+main();
